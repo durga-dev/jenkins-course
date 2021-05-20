ee3967c13e27fe8f5760a80831b4771f79fbf080
Durga Ballabha Panigrahi <panigrahi.durga9@gmail.com>
2021-05-20 15:25:42 +0530

    Create nodejs.groovy

---
+++ job-dsl/nodejs.groovy
@@ -0,0 +1,18 @@
+job('NodeJS example') {
+    scm {
+        git('git://github.com/durga-dev/docker-demo.git') {  node -> 
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs') 
+                         
+    }
+    steps {
+        shell("npm install")
+    }
+}
